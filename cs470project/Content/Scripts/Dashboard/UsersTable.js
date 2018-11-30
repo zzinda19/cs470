f1dda1c72aacf53706caa957899e499c23df02db
Zachary Zinda <zzinda19@jcu.edu>
2018-11-30 16:05:00 -0500

    Fixed AddUser form to Fit Inside BootBox Modal with working Typeahead.

--- cs470project/Content/Scripts/Dashboard/UsersTable.js
+++ cs470project/Content/Scripts/Dashboard/UsersTable.js
@@ -1,117 +1,157 @@
-﻿var projectId = $("#projectID").val();
+﻿var userDataTable;
+var researchProjectUserDto = {};
 
-var userTable = {
-    Initialize: function () {
-        // Create User DataTable Using Ajax
-        var table = $("#researchProjectUsers").DataTable({
-            ajax: {
-                // Call GetResearchProjectUsers from the Api Users Controller
-                url: "/Api/ResearchUsers/" + projectId,
-                dataSrc: "",
-                error: function (xhr) {
-                    toastr.error("An error occured: " + xhr.status + " " + xhr.statusText);
+function SetupUserDataTable(projectId) {
+    userDataTable = $("#researchProjectUsers").DataTable({
+        ajax: {
+            // Calls GetResearchProjectUsers in the Api ResearchUsers Controller.
+            url: "/Api/ResearchUsers/" + projectId,
+            dataSrc: "",
+            error: function (xhr) {
+                toastr.error("An error occured: " + xhr.status + " " + xhr.statusText);
+            }
+        },
+        columns: [
+            {
+                data: "researchUser.username"
+            },
+            {
+                data: "admin",
+                render: function (data) {
+                    // Admin is of boolean type.
+                    if (data == true) {
+                        return "Admin";
+                    } else {
+                        return "User";
+                    }
                 }
             },
-            columns: [
-                {
-                    // Fill column 1 with the users username.
-                    data: "researchUser.username"
-                },
-                {
-                    // Fill column 2 with the users admin status.
-                    data: "admin",
-                    render: function (data) {
-                        // Admin is a boolean value. Convert true to Admin, false to User.
-                        if (data == true) {
-                            return "Admin";
-                        } else {
-                            return "User";
-                        }
-                    }
-                },
-                {
-                    // Fill column 3 with a remove link that contains the users id.
-                    data: "researchUser.userId",
-                    render: function (data) {
-                        return "<button class='btn-link js-delete' data-user-id=" + data + ">Remove</button>";
-                    }
+            {
+                data: "researchUser.userId",
+                render: function (data) {
+                    return "<button class='btn-link js-delete' data-user-id=" + data + ">Remove</button>";
                 }
-            ]
-        });
+            }
+        ]
+    });
+}
 
-        // Set remove button on click handler.
-        table.on("click", ".js-delete", function () {
-            var button = $(this);
-            // Pull the users id from the remove button's data-user-id attribute.
-            var userId = button.attr("data-user-id");
-            // If the program user confirms, remove selected user from the research project.
-            bootbox.confirm("Are you sure you want to remove this user from the project?", function (result) {
-                if (result) {
+function SetupRemoveButtonOnClickHandler(projectId) {
+    userDataTable.on("click", ".js-delete", function () {
+        var removeButton = $(this);
+        // Pull userId from the remove button's data-user-id attribute.
+        var userId = removeButton.attr("data-user-id");
+
+        bootbox.confirm("Are you sure you want to remove this user from the project?", function (result) {
+            if (result) {
+                $.ajax({
+                    // Calls DeleteResearchProjectUser in the Api ResearchUsers Controller.
+                    url: "/Api/ResearchUsers/" + projectId + "/" + userId,
+                    method: "DELETE",
+                    success: function () {
+                        // If successful remove the user's row and redraw DataTable.
+                        userDataTable.row(removeButton.parents("tr")).remove().draw();
+                    },
+                    error: function (xhr) {
+                        toastr.error("An error occured: " + xhr.status + " " + xhr.statusText);
+                    }
+                });
+            }
+        });
+    });
+}
+
+function GetResearchUsers() {
+    // Uses Bloodhound Package to Query ResearchUsers as the User Inputs a Username
+    return new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/Api/Users?query=%QUERY',
+            wildcard: '%QUERY'
+        }
+    });
+}
+
+function SetupAddUserModalForm(form) {
+    // Creates the form modal using the Bootbox Package.
+    var addUserModal = bootbox.dialog({
+        title: "Add User",
+        // Must pass in form not form.html() otherwise the Typeahead package cannot find the input field by id.
+        message: form,
+        buttons: {
+            cancel: {
+                label: "Cancel",
+                className: "btn btn-default",
+                callback: function () {
+                    // Clear username input field when modal is dismissed.
+                    $("#username").typeahead("val", "");
+                }
+            },
+            ok: {
+                label: "Submit",
+                className: "btn btn-primary",
+                callback: function () {
+                    // Clear username input field when modal is dismissed.
+                    $("#username").typeahead("val", "");
+
+                    // Pull admin value from form and add to Dto.
+                    var admin = $("input[name='admin']:checked").val();
+                    researchProjectUserDto.admin = admin;
+
+                    // Transfer the ResearchProjectUserDto to the Api Controller using Ajax.
                     $.ajax({
-                        // Call DeleteResearchProjectUser from the Api Users Controller.
-                        url: "/Api/ResearchUsers/" + projectId + "/" + userId,
-                        method: "DELETE",
+                        // Calls AddResearchUserToProject in the Api ResearchUsers Controller.
+                        url: "/Api/ResearchUsers/Add",
+                        method: "POST",
+                        data: researchProjectUserDto,
                         success: function () {
-                            // If successful delete row and redraw DataTable.
-                            table.row(button.parents("tr")).remove().draw();
+                            userDataTable.ajax.reload();
                         },
                         error: function (xhr) {
                             toastr.error("An error occured: " + xhr.status + " " + xhr.statusText);
                         }
                     });
                 }
-            });
-        });
-
-        var vm = {
-            projectId: projectId,
-        };
-
-        var researchUsers = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            remote: {
-                url: '/Api/Users?query=%QUERY',
-                wildcard: '%QUERY'
             }
-        });
+        }
+    });
 
-        $('#username').typeahead({
-            minLength: 3,
-            highlight: true
-        }, {
-                name: 'researchUsers',
-                display: 'username',
-                source: researchUsers
+    // Must use .init to initialize Typeahead. Otherwise if the modal is reopened Typeahead doesn't work.
+    addUserModal.init(function () {
+        // Get the list of ResearchUsers using Bloodhound.
+        var researchUsers = GetResearchUsers();
+        // Set the Typeahead parameters for the username field.
+        $("#username").typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 3
+        },
+        {
+            name: 'researchUsers',
+            display: 'username',
+            source: researchUsers
             }).on("typeahead:select", function (e, researchUser) {
-                vm.userId = researchUser.userId;
-            });
-
-        var form = $("#addUserForm");
-        form.on("submit", function (e) {
-
-            e.preventDefault();
-            $("#username").typeahead("val", "");
-
-            var admin = $("input[name='admin']:checked").val();
-            vm.admin = admin;
-
-            $.ajax({
-                url: "/Api/ResearchUsers/Add",
-                method: "POST",
-                data: vm,
-                success: function () {
-                    table.ajax.reload();
-                },
-                error: function (xhr) {
-                    toastr.error("An error occured: " + xhr.status + " " + xhr.statusText);
-                }
-            });
+            // Once a user is selected, add their id to the Dto.
+            researchProjectUserDto.userId = researchUser.userId;
         });
-    }
+    });
+}
+
+function SetupAddUserButtonOnClickHandler(projectId) {
+    // AddUserForm must be set upon page initialization and passed to the BootBox modal.
+    // Otherwise, the modal has no way to access the form if the modal is reopened.
+    var addUserForm = $("#addUserForm");
+    var addUserButton = $("#addUserButton");
+    addUserButton.click(function () {
+        researchProjectUserDto.projectId = projectId;
+        SetupAddUserModalForm(addUserForm);
+    });
 }
 
 $(document).ready(function () {
-    userTable.Initialize();
-    userForm.Initialize();
+    var projectId = $("#projectID").val();
+    SetupUserDataTable(projectId);
+    SetupRemoveButtonOnClickHandler(projectId);
+    SetupAddUserButtonOnClickHandler(projectId);
 });
