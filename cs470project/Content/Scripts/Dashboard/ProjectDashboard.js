3f6c439f3e20b577c1d996c67a227686469ca669
Landry Snead <lsnead18@jcu.edu>
2018-12-04 23:50:16 -0500

    add validation functionality

--- cs470project/Content/Scripts/Dashboard/ProjectDashboard.js
+++ cs470project/Content/Scripts/Dashboard/ProjectDashboard.js
@@ -1,4 +1,10 @@
-﻿var layout = {
+﻿/**
+*  Author:          Zak Zinda
+*  Updated By:      
+*  Date Updated:    10.26.18
+*  Description:     This populates the project dashboard with the appropriate project information
+*/
+var layout = {
     Initialize: function () {
         layout.PopulateDashboard();
     },
@@ -20,6 +26,13 @@
     }
 };
 
+
+/**
+*  Author:          Zak Zinda
+*  Updated By:      Landry Snead
+*  Date Updated:    12.4.18
+*  Description:     This handles the client side end of uploading
+*/
 var uploadForm = {
 
     Initialize: function () {
@@ -31,6 +44,7 @@ var uploadForm = {
         var fileInput = $("#fileInput");
         $("#fileName").val("");
 
+        //display selected file name
         fileInput.on("change", function () {
             console.log(fileInput);
             var file = fileInput[0].files[0];
@@ -40,12 +54,16 @@ var uploadForm = {
         });
     },
 
+
     SetUploadFileHandler: function () {
         var id = $("#projectID").val();
-        
+
         $("#uploadForm").on("submit", function (e) {
             // Prevent the default form action so that the page is not refreshed upon user upload.
             e.preventDefault();
+
+            //display upload spinner to indicate uploading is in progress
+            document.getElementById("uploadSpinner").style.fontSize = "24px";
 
             // Obtain the inputted file.
             var fileInput = $("#fileInput");
@@ -68,6 +86,31 @@ var uploadForm = {
                 processData: false,
                 success: function (data) {
                     toastr.success("Accession numbers successfully uploaded.");
+
+                    //remove upload spinner
+                    document.getElementById("uploadSpinner").style.fontSize = "0px";
+
+                    if (data.length > 0) {
+                        //display rejected accession numbers
+                        document.getElementById("uploadedMessage").innerHTML = "Some of your accession numbers may have been rejected. Reference this table to see why";
+                        document.getElementById("researchAccessions").style.display = "block";
+
+                        //clear data if a table already exists
+                        if ($.fn.DataTable.isDataTable("#researchAccessions")) {
+                            $('#researchAccessions').DataTable().clear().destroy();
+                        }
+
+                        $("#researchAccessions").DataTable({
+                            data: data,
+                            columns: [
+                                { data: "accession" },
+                                { data: "reason" }
+                            ],
+                            "fnCreatedRow": function (nRow) {
+                                $(nRow).attr('class', 'danger');
+                            }
+                        });
+                    }
                     console.log(data);
                 },
                 error: function (xhr) {
@@ -78,10 +121,21 @@ var uploadForm = {
             // Prevent page refresh upon submit.
             return false;
         });
+
     }
 };
 
+
+/**
+*  Author:          Zak Zinda
+*  Updated By:      
+*  Date Updated:    10.26.18
+*  Description:     On page load hide elements and intialize the layout and uploadForm functions
+*/
 $(document).ready(function () {
+    //when the page is loaded, do not display the rejected accessions table or upload spinner
+    document.getElementById("researchAccessions").style.display = "none";
+    document.getElementById("uploadSpinner").style.fontSize = "0px";
     layout.Initialize();
     uploadForm.Initialize();
 });
--- cs470project/Controllers/Api/FileController.cs
+++ cs470project/Controllers/Api/FileController.cs
@@ -12,24 +12,26 @@ using cs470project.Models;
 using cs470project.Dtos;
 using System.Text;
 using System.Net.Http.Headers;
+using System.Data;
 
 namespace cs470project.Controllers.Api
 {
     public class FileController : ApiController
     {
-
         /**
-         *  Author: Zak Zinda
+         *  Author:       Zak Zinda
          *  Date Updated: 10.29.18
-         *  Description: Top-level method for adding user-uploaded accession numbers
-         *               to the project database.
-         *  Parameters: int id - Project ID, passed through hidden form in ProjectDashboard.cshtml,
-         *                       included so that the accession numbers are added to the correct project.
+         *  Updated By:   Landry Snead
+         *  Description:  Top-level method for adding user-uploaded accession numbers
+         *                to the project database.
+         *  Parameters:   int id - Project ID, passed through hidden form in ProjectDashboard.cshtml,
+         *                         included so that the accession numbers are added to the correct project.
          */
         // POST: /Api/File/1
         [HttpPost]
         public async Task<IHttpActionResult> Upload(int id)
         {
+
             if (!Request.Content.IsMimeMultipartContent())
             {
                 return BadRequest("File improperly formatted.");
@@ -45,11 +47,13 @@ namespace cs470project.Controllers.Api
                 var file = provider.FileData[0];
                 string fileName = file.LocalFileName;
 
-                string hasHeaderFormValue = provider.FormData.GetValues("hasHeader")[0];                
+                string hasHeaderFormValue = provider.FormData.GetValues("hasHeader")[0];
                 bool hasHeader = hasHeaderFormValue.Contains("yes") ? true : false;
 
                 FileUploadHelper helper = new FileUploadHelper();
                 List<string> accessionNumbers = helper.ParseFileToList(fileName, hasHeader);
+
+                DataTable rejectedAccessions = new DataTable();
 
                 using (var context = new CCFDataEntities())
                 {
@@ -60,20 +64,10 @@ namespace cs470project.Controllers.Api
                         return BadRequest("The selected research project does not exist.");
                     }
 
-                    foreach(var accessionNumber in accessionNumbers)
-                    {
-                        var accession = new ResearchProjectAccession()
-                        {
-                            ResearchProject = researchProjectInDb,
-                            Accession = Convert.ToInt32(accessionNumber),
-                            AlternateGUID = Guid.NewGuid()
-                        };
-                        context.ResearchProjectAccessions.Add(accession);
-                        context.SaveChanges();
-                    }
+                    rejectedAccessions = ValidateUploadedAccessions(accessionNumbers, context, id, researchProjectInDb);
                 }
 
-                return Ok(accessionNumbers);
+                return Ok(rejectedAccessions);
             }
             catch (InvalidCastException e)
             {
@@ -83,6 +77,103 @@ namespace cs470project.Controllers.Api
             {
                 return InternalServerError(e);
             }
+        }
+
+        /**
+         *  Author:       Landry Snead
+         *  Date Updated: 11.10.18
+         *  Description:  This checks to see if the accession is already in the project
+         *  Parameters:   string accession: the accession number to check (from the uploaded list)
+         *                CCFDataEntities context: the database connection
+         *                int id: the id of the project to check in
+         */
+        public Boolean AccessionIsDuplicate(string accession, CCFDataEntities context, int id)
+        {
+            int accessionAsInt = Convert.ToInt32(accession);
+            Boolean accessionExists = context.ResearchProjectAccessions
+                                       .Where(a => a.Accession == accessionAsInt && a.ProjectID == id)
+                                       .Any();
+            return accessionExists;
+        }
+
+        /**
+         *  Author:       Landry Snead
+         *  Date Updated: 11.26.18
+         *  Description:  This checks to see if the accession exists. To be implemented...
+         *  Parameters:   string accession: the accession number to check (from the uploaded list)
+         *                CCFDataEntities context: the database connection
+         *                int id: the id of the project to check in
+         */
+        /*
+       public Boolean AccessionExists(string accession, CCFDataEntities context, int id)
+       {
+           int accessionAsInt = Convert.ToInt32(accession);
+           Boolean accessionExists = context.ResearchProjectAccessions
+                                      .Where(a => a.Accession == accessionAsInt && a.ProjectID == id)
+                                      .Any();
+           return accessionExists;
+       }
+       /*
+
+       /**
+        *  Author:       Landry Snead
+        *  Date Updated: 11.10.18
+        *  Description:  This checks each uploaded accession number to see if it is valid. 
+        *                  Step 1: is it in the correct format (an integer value)?
+        *                  Step 2: is it already included in the project?
+        *                  Step 3: does the accession exist? --not implemented 
+        *                  Then is adds the accession to the project.
+        *  Parameters:   List<string> accessions: A string list of accession numbers from the uploaded file.
+        *                CCFDataEntities context: the database connection
+        *                int id: the id of project to add to
+        *                ResearchProject researchProjectInDb: The research project object
+        */
+        [HttpGet]
+        public DataTable ValidateUploadedAccessions(List<string> accessions, CCFDataEntities context, int id, ResearchProject researchProjectInDb)
+        {
+            DataTable rejected = new DataTable();
+            rejected.Columns.Add("accession", typeof(string));
+            rejected.Columns.Add("reason", typeof(string));
+
+            foreach (var accessionNumber in accessions)
+            {
+                //check if in the correct format
+                if (!accessionNumber.All(char.IsDigit))
+                {
+                    //accession is not an int, add to rejected
+                    rejected.Rows.Add(accessionNumber, "Accession must be an integer");
+                    continue;
+                }
+
+                //check if duplicate
+                else if (AccessionIsDuplicate(accessionNumber, context, id))
+                {
+                    //accession is already a part of the project, add to rejected
+                    rejected.Rows.Add(accessionNumber, "Accession is already included in the project");
+                    continue;
+                }
+
+                //check if in global database --to be implemented
+                //else if (accessionExists(accessionNumber, context,id))
+                //{
+                //    //accession does not exist in the global database, add to rejected
+                //    rejected.Rows.Add(accessionNumber, "Accession does not exist");
+                //    continue;
+                //}
+
+                //accession passed validation, create an accession object to add to the database
+                var accession = new ResearchProjectAccession()
+                {
+                    ResearchProject = researchProjectInDb,
+                    Accession = Convert.ToInt32(accessionNumber.Trim()),
+                    AlternateGUID = Guid.NewGuid() //deidentified accession number
+                };
+
+                context.ResearchProjectAccessions.Add(accession);
+                context.SaveChanges();
+            }
+
+            return rejected;
         }
 
         // GET: /Api/File/1
@@ -121,7 +212,7 @@ namespace cs470project.Controllers.Api
                             .Select(Mapper.Map<ResearchProjectAccession, AccessionDto>);
 
                         sb.Append("Accession,AlternateGuid\r\n");
-                        foreach(var keyPair in accessionKeyPairs)
+                        foreach (var keyPair in accessionKeyPairs)
                         {
                             sb.AppendFormat("=\"{0}\",", keyPair.Accession.ToString());
                             sb.AppendFormat("=\"{0}\"\r\n", keyPair.AlternateGuid.ToString());
@@ -129,7 +220,7 @@ namespace cs470project.Controllers.Api
                         break;
                     case DownloadType.MRNOnly:
                         fileName = "MRNKeyPairs.csv";
-                        
+
                         break;
                     case DownloadType.Both:
                         fileName = "AccessionAndMRNKeyPairs.csv";
@@ -152,7 +243,7 @@ namespace cs470project.Controllers.Api
                 var response = ResponseMessage(result);
 
                 return response;
-            }     
+            }
         }
     }
-}
+}   
